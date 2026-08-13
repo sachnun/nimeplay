d73254088faf3ab2d76bbd19f225faadd1a49c17
sachnun <sachnun12@gmail.com>
2026-08-14 02:19:44 +0700

    feat: deep-link continue episode

--- app/components/AnimeInfiniteGrid.vue
+++ app/components/AnimeInfiniteGrid.vue
@@ -85,7 +85,7 @@ const displayCards = computed(() => displayAnime.value.map(({ anime, isFromNext 
     isFromNext,
     progress,
     badge: episodeBadge(anime.episode),
-    to: `/${anime.slug}`,
+    to: progress ? `/${anime.slug}/${progress.episodeNum}` : `/${anime.slug}`,
   }
 }))
 const hasAnyCard = computed(() => displayAnime.value.length > 0 || props.continueItems.length > 0)
@@ -148,7 +148,7 @@ function episodeBadge(episode: string) {
       <NuxtLink
         v-for="(item, i) in continueItems"
         :key="`continue-${item.animeSlug}`"
-        :to="`/${item.animeSlug}`"
+        :to="`/${item.animeSlug}/${item.episodeNum}`"
         class="block rounded-lg overflow-hidden bg-card relative outline-none group hover:border-accent focus:border-accent hover:z-10 focus:z-10"
         @pointerdown="onProgressCardPointerDown($event, item.animeSlug)"
         @pointermove="onProgressCardPointerMove"

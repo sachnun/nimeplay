fc08ab2f61ff130989dad3f3de55125a9e4fe166
sachnun <sachnun12@gmail.com>
2026-08-27 23:43:55 +0000

    fix: drop worker limits on free plan

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,9 +19,6 @@ export default defineNuxtConfig({
       wrangler: {
         name: 'nimeplay',
         compatibility_date: '2025-07-15',
-        limits: {
-          subrequests: 1000,
-        } as unknown as { cpu_ms?: number },
       },
     },
     experimental: { openAPI: true },

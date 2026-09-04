96a4cc02f262da526c59ebe91d25737906b3d807
sachnun <sachnun12@gmail.com>
2026-09-05 05:59:57 +0700

    fix: use valid placement hint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,7 +24,7 @@ export default defineNuxtConfig({
         compatibility_date: '2025-07-15',
         placement: {
           mode: 'smart',
-          hint: 'gcp:asia-southeast2',
+          hint: 'apac',
         },
       },
     },

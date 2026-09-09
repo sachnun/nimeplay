788401285d2aabc89f48f40a6eb610e72e99100c
sachnun <sachnun12@gmail.com>
2026-09-09 19:44:32 +0000

    Revert "fix: sort ongoing anime by day"
    This reverts commit 4f5cf07b8df073cecf373ae08d3a9fb577c7b25d.

--- server/utils/queries.ts
+++ server/utils/queries.ts
@@ -98,16 +98,6 @@ const SEASON_YEAR = sql`case
   then cast(substr(${anime.season}, -4) as integer)
   else 0 end`
 
-const DAY_ORDER = sql`case ${anime.day}
-  when 'Senin' then 1
-  when 'Selasa' then 2
-  when 'Rabu' then 3
-  when 'Kamis' then 4
-  when 'Jumat' then 5
-  when 'Sabtu' then 6
-  when 'Minggu' then 7
-  else 8 end`
-
 export function listAnimePage(status: 'ONGOING' | 'COMPLETED', page: number): Promise<{ anime: AnimeCard[], totalPages: number }> {
   return cache.get('list', `${status}:${page}`, LIST_TTL_MS, () => listAnimePageFresh(status, page)) as Promise<{ anime: AnimeCard[], totalPages: number }>
 }
@@ -119,7 +109,7 @@ async function listAnimePageFresh(
   const filter = and(eq(anime.status, status), METADATA_READY)
 
   const orderBy = status === 'ONGOING'
-    ? [asc(DAY_ORDER), sql`${anime.lastNewEpisodeAt} desc nulls last`, sql`${anime.ongoingRank} asc nulls last`, asc(anime.malId)]
+    ? [sql`${anime.lastNewEpisodeAt} desc nulls last`, sql`${anime.ongoingRank} asc nulls last`, sql`${anime.latestEpisodeAt} desc nulls last`, desc(anime.updatedAt)]
     : [desc(SEASON_YEAR), desc(SEASON_RANK), desc(anime.updatedAt)]
 
   const countQuery = db()

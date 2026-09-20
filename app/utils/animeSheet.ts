fea3be6c79b840651e8dfb0e3dadde36db20c690
Sachnun <sachnun12@gmail.com>
2026-09-20 17:18:03 +0700

    fix: player back to anime detail (#50)

--- app/utils/animeSheet.ts
+++ app/utils/animeSheet.ts
@@ -2,8 +2,6 @@ export const ANIME_SHEET_QUERY = '(max-width: 1023px)'
 
 export const ANIME_SHEET_DETAIL_RE = /^\/anime\/[^/]+\/?$/
 
-export const ANIME_SHEET_EPISODE_RE = /^\/anime\/[^/]+\/[^/]+\/?$/
-
 export function isAnimeSheetViewport() {
   return import.meta.client && typeof window !== 'undefined' && window.matchMedia(ANIME_SHEET_QUERY).matches
 }

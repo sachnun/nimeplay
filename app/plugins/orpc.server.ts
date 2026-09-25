b66b62eab1b673909e263641c0c5849d18f6a020
sachnun <sachnun12@gmail.com>
2026-09-26 02:33:53 +0700

    fix: eager orpc context origin

--- app/plugins/orpc.server.ts
+++ app/plugins/orpc.server.ts
@@ -5,7 +5,7 @@ import type { AppRouter } from '~~/server/orpc/router'
 
 export default defineNuxtPlugin(() => {
   const client: RouterClient<AppRouter> = createRouterClient(router, {
-    context: () => ({ origin: useRequestURL().origin }),
+    context: { origin: useRequestURL().origin },
   })
   return { provide: { orpc: client } }
 })
